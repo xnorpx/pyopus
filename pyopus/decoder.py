eb4da731d1fbe88a0a06e1029744d6791db6ee41
Wang Xuerui <idontknw.wang@gmail.com>
2014-03-18 02:46:26 +0800

    implement CTL interface on OpusDecoder

--- pyopus/decoder.py
+++ pyopus/decoder.py
@@ -10,6 +10,7 @@ __all__ = [
 
 from . import base
 from . import llinterface
+from . import ctl
 from . import utils
 
 
@@ -32,6 +33,29 @@ class OpusDecoder(base.OpusCodec):
 
     def _init_state(self):
         llinterface.decoder_init(self._state, self.frequency, self.channels)
+
+    def reset_state(self):
+        ctl.decoder_reset_state(self._state)
+
+    @property
+    def final_range(self):
+        return ctl.decoder_get_final_range(self._state)
+
+    @property
+    def pitch(self):
+        return ctl.decoder_get_pitch(self._state)
+
+    @property
+    def bandwidth(self):
+        return ctl.decoder_get_bandwidth(self._state)
+
+    @property
+    def gain(self):
+        return ctl.decoder_get_gain(self._state)
+
+    @gain.setter
+    def gain(self, value):
+        ctl.decoder_set_gain(self._state, value)
 
 
 # vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
